b265d714842c0c12cc2ee60007e10d13bf4bfff9
HoraryZero <rnd.programmer.mike@gmail.com>
2022-01-01 15:14:46 +0300

    Добавьте файлы проекта.

---
+++ smarthome-arduino/smarthome-arduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		smarthome_arduino.ino
+ Created:	01.01.2022
+ Author:	HoraryZero
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
